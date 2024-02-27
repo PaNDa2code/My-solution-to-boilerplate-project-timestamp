5e22517d57f73cb29414984a83e3a82bfa3c346c
PaNDa <126345041+PaNDa2code@users.noreply.github.com>
2024-02-27 00:28:47 -0500

    Solved

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,8 +21,38 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
+
+app.get("/api/:date", (req, res) => {
+  let dateRegex = /[[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}/
+  let unixRegex = /[0-9]{13}/
+  let date;
+  if (req.params.date.match(dateRegex)) {
+    date = new Date(req.params.date)
+  }
+  else if (req.params.date.match(unixRegex)) {
+    date = new Date(Number(req.params.date))
+  }
+  else {
+    res.json({ error: "Invalid Date" });
+    return;
+  }
+  const unix = date.getTime();
+  const utc = date.toUTCString();
+  res.json({
+    unix: unix,
+    utc: utc
+  })
+});
+
+app.get("/api", (req, res) => {
+  let date = new Date();
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+})
 
 
 
